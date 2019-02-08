47210fa50af4197543966d61ecba06f02c76c65d
RumenRT <33309rt@gmail.com>
2019-02-08 17:02:40 -0500

    sandbox final

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -19,6 +19,16 @@ VARIABLE RULES:
 # name = 'Brad'
 # is_cool = True
 
-#multiple assignment
+# multiple assignment
 x, y, name, is_cool = (1, 2.5, "Brad", True)
-print x
+print (x, y, name, is_cool)
+
+# basic math
+a = x + y
+
+# casting
+x = str(x)
+y = int(y)
+
+# check type
+print (type(y))
